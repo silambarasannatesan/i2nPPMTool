f069c0e13a00bf9af94c5e19c8891b9c31f32006
Silambarasan N <silambarasanasn@gmail.com>
2021-04-03 12:45:28 +0530

    Added delete project by id code

--- PPMTool_Backend/src/main/java/com/i2n/ppmtool/services/ProjectService.java
+++ PPMTool_Backend/src/main/java/com/i2n/ppmtool/services/ProjectService.java
@@ -32,4 +32,12 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete project with id" + projectId + "This project does not exists");
+        }
+        projectRepository.delete(project);
+    }
 }
--- PPMTool_Backend/src/main/java/com/i2n/ppmtool/web/ProjectController.java
+++ PPMTool_Backend/src/main/java/com/i2n/ppmtool/web/ProjectController.java
@@ -44,4 +44,10 @@ public class ProjectController {
     public Iterable<Project> getAllProjects() {
         return projectService.findAllProjects();
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with Id" + projectId + "was deleted succsessfully", HttpStatus.OK);
+    }
 }
